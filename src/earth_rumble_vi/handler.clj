eec46b8e944678da2898fd57f5dc69855ad903fc
Eric Hees <eric.hees@gmail.com>
2013-02-04 21:33:49 -0500

    actually made project this time

---
+++ src/earth_rumble_vi/handler.clj
@@ -0,0 +1,11 @@
+(ns earth-rumble-vi.handler
+  (:use compojure.core)
+  (:require [compojure.handler :as handler]
+            [compojure.route :as route]))
+
+(defroutes app-routes
+  (GET "/" [] "Hello World")
+  (route/not-found "Not Found"))
+
+(def app
+  (handler/site app-routes))
