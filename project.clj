93f2d702a64a7d4cad4b2fc7a3d686ffa40c1d29
Eric Hees <eric.hees@gmail.com>
2013-02-07 00:56:56 -0500

    can create contests

--- project.clj
+++ project.clj
@@ -2,8 +2,12 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.4.0"]
+                 [markdown-clj "0.9.19"]
+                 [hiccup "1.0.2"]
+                 [lib-noir "0.3.5"]
                  [compojure "1.1.5"]]
   :plugins [[lein-ring "0.8.2"]]
   :ring {:handler earth-rumble-vi.handler/app}
   :profiles
-  {:dev {:dependencies [[ring-mock "0.1.3"]]}})
+  {:dev {:dependencies [[lein-cljsbuild "0.3.0"]
+                        [ring-mock "0.1.3"]]}})
--- src/earth_rumble_vi/handler.clj
+++ src/earth_rumble_vi/handler.clj
@@ -1,10 +1,17 @@
 (ns earth-rumble-vi.handler
-  (:use compojure.core)
+  (:use compojure.core
+        hiccup.core)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]))
 
+(defn hello-user
+  [id]
+  (prn id)
+  (html
+    [:p "hello " id]))
+
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/:id" [id] (hello-user id))
   (route/not-found "Not Found"))
 
 (def app
